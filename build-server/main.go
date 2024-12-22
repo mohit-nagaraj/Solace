431238636897d71fafcf9131d4c652a200cebc8d
mohit-nagaraj <mohitnagaraj20@gmail.com>
2024-12-22 13:42:52 +0530

    env update

--- build-server/main.go
+++ build-server/main.go
@@ -15,6 +15,7 @@ import (
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/credentials"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/joho/godotenv"
 )
 
 // runCommand executes a shell command and captures its output.
@@ -60,7 +61,19 @@ func uploadFile(ctx context.Context, client *s3.Client, bucket string, key strin
 }
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 	ctx := context.Background()
+
+	AWS_ACCESS_KEY_ID := os.Getenv("AWS_ACCESS_KEY_ID")
+	AWS_SECRET_ACCESS_KEY := os.Getenv("AWS_SECRET_ACCESS_KEY")
+
+	if AWS_ACCESS_KEY_ID == "" || AWS_SECRET_ACCESS_KEY == "" {
+		log.Fatalf("AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY are required.")
+		return
+	}
 
 	projectID := flag.String("pid", "", "Project ID to use for S3 key")
 	bucket := "solace-outputs"
@@ -68,6 +81,7 @@ func main() {
 
 	if *projectID == "" {
 		log.Fatalf("Project ID is required.")
+		return
 	}
 
 	fmt.Println("Build Started...")
@@ -75,32 +89,40 @@ func main() {
 
 	if err := runCommand(ctx, fmt.Sprintf("git clone %s ./output", repoLink)); err != nil {
 		log.Fatalf("Failed to clone repository: %v", err)
+		return
 	}
 	fmt.Println("Repository cloned.")
 
 	outDirPath := filepath.Join(".", "output")
-	if err := runCommand(ctx, fmt.Sprintf("cd %s && npm install && npm run build", outDirPath)); err != nil {
+	if err := runCommand(ctx, fmt.Sprintf("cd %s", outDirPath)); err != nil {
 		log.Fatalf("Build failed: %v", err)
+		return
+	}
+
+	if err := runCommand(ctx, "npm install && npm run build"); err != nil {
+		log.Fatalf("Build 2 failed: %v", err)
+		return
 	}
 
 	distFolderPath := filepath.Join(outDirPath, "dist")
 	files, err := os.ReadDir(distFolderPath)
 	if err != nil {
 		log.Fatalf("Failed to read dist folder: %v", err)
+		return
 	}
 
 	cfg, err := config.LoadDefaultConfig(
-
 		context.TODO(),
 		config.WithCredentialsProvider(credentials.NewStaticCredentialsProvider(
-			"YOUR_ACCESS_KEY_ID",
-			"YOUR_SECRET_ACCESS_KEY",
-			"", // token
+			AWS_ACCESS_KEY_ID,
+			AWS_SECRET_ACCESS_KEY,
+			"",
 		)),
-		config.WithRegion("your-region"),
+		config.WithRegion("ap-south-1"),
 	)
 	if err != nil {
 		log.Fatalf("Failed to load AWS config: %v\n", err)
+		return
 	}
 	client := s3.NewFromConfig(cfg)
 	fmt.Println("Initialized S3 client.")
