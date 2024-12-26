b61b8ee52bf70e6944bf1c98822c9c9dfe801a16
mohit-nagaraj <mohitnagaraj20@gmail.com>
2024-12-26 12:18:00 +0530

    Frontend integration

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,101 +1,120 @@
-import Image from "next/image";
+"use client";
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
+import { io } from "socket.io-client";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Github } from "lucide-react";
+import { Fira_Code } from "next/font/google";
+import axios from "axios";
+
+const socket = io("http://localhost:9002");
+
+const firaCode = Fira_Code({ subsets: ["latin"] });
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [repoURL, setURL] = useState<string>("");
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+  const [logs, setLogs] = useState<string[]>([]);
+
+  const [loading, setLoading] = useState(false);
+
+  const [projectId, setProjectId] = useState<string | undefined>();
+  const [deployPreviewURL, setDeployPreviewURL] = useState<
+    string | undefined
+  >();
+
+  const logContainerRef = useRef<HTMLElement>(null);
+
+  const isValidURL: [boolean, string | null] = useMemo(() => {
+    if (!repoURL || repoURL.trim() === "") return [false, null];
+    const regex = new RegExp(
+      /^(?:https?:\/\/)?(?:www\.)?github\.com\/([^\/]+)\/([^\/]+)(?:\/)?$/
+    );
+    return [regex.test(repoURL), "Enter valid Github Repository URL"];
+  }, [repoURL]);
+
+  const handleClickDeploy = useCallback(async () => {
+    setLoading(true);
+
+    const { data } = await axios.post(`http://localhost:9000/project`, {
+      gitURL: repoURL,
+      slug: projectId,
+    });
+
+    if (data && data.data) {
+      const { projectSlug, url } = data.data;
+      setProjectId(projectSlug);
+      setDeployPreviewURL(url);
+
+      console.log(`Subscribing to logs:${projectSlug}`);
+      socket.emit("subscribe", `logs:${projectSlug}`);
+    }
+  }, [projectId, repoURL]);
+
+  const handleSocketIncommingMessage = useCallback((message: string) => {
+    console.log(`[Incomming Socket Message]:`, typeof message, message);
+    const { log } = JSON.parse(message);
+    setLogs((prev) => [...prev, log]);
+    logContainerRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, []);
+
+  useEffect(() => {
+    socket.on("message", handleSocketIncommingMessage);
+
+    return () => {
+      socket.off("message", handleSocketIncommingMessage);
+    };
+  }, [handleSocketIncommingMessage]);
+
+  return (
+    <main className="flex justify-center items-center h-[100vh]">
+      <div className="w-[600px]">
+        <span className="flex justify-start items-center gap-2">
+          <Github className="text-5xl" />
+          <Input
+            disabled={loading}
+            value={repoURL}
+            onChange={(e) => setURL(e.target.value)}
+            type="url"
+            placeholder="Github URL"
+          />
+        </span>
+        <Button
+          onClick={handleClickDeploy}
+          disabled={!isValidURL[0] || loading}
+          className="w-full mt-3"
+        >
+          {loading ? "In Progress" : "Deploy"}
+        </Button>
+        {deployPreviewURL && (
+          <div className="mt-2 bg-slate-900 py-4 px-2 rounded-lg">
+            <p>
+              Preview URL{" "}
+              <a
+                target="_blank"
+                className="text-sky-400 bg-sky-950 px-3 py-2 rounded-lg"
+                href={deployPreviewURL}
+              >
+                {deployPreviewURL}
+              </a>
+            </p>
+          </div>
+        )}
+        {logs.length > 0 && (
+          <div
+            className={`${firaCode.className} text-sm text-green-500 logs-container mt-5 border-green-500 border-2 rounded-lg p-4 h-[300px] overflow-y-auto`}
           >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+            <pre className="flex flex-col gap-1">
+              {logs.map((log, i) => (
+                <code
+                  ref={logs.length - 1 === i ? logContainerRef : undefined}
+                  key={i}
+                >{`> ${log}`}</code>
+              ))}
+            </pre>
+          </div>
+        )}
+      </div>
+    </main>
   );
 }
